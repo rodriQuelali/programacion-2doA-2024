6747f2b284b3d744b3f8aab9d78fd4243d0f4ada
Rodri Quelali <rodriquelali@gmail.com>
2024-04-03 18:53:04 -0400

    llmado de otra actividad con Intent

--- MyApplicationComponentes/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplicationComponentes/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button tnh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //casting
+        //parsing
+
     }
+
+    //realizar la codificacion para el llamado a otra actividad.
+    //Objeto llamado Intent.
+
+    public void lanzar(View view){
+        Intent intent = new Intent(this, registro.class);
+        startActivity(intent);
+    }
+
 }
