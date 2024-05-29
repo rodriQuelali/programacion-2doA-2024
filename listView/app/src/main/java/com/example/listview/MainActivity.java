397da682bf0a031fb2337fc1e89bcc3cb5834722
Rodri Quelali <rodriquelali@gmail.com>
2024-05-29 11:26:11 -0400

    listViews

--- listView/app/src/main/java/com/example/listview/MainActivity.java
+++ listView/app/src/main/java/com/example/listview/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 	private TextView tvH;
 	private ListView lvP;
+	private EditText txtA;
+	private Button btnA;
+	ArrayList paises = new ArrayList();
+	ArrayAdapter<String> arrayAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +27,39 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		cargarR();
         estadoListView();
+		agragrPaises();
 	}
 	public void cargarR(){
 		tvH = (TextView) findViewById(R.id.tvHabitantes);
 		lvP = (ListView) findViewById(R.id.lvPaises);
+		txtA = (EditText) findViewById(R.id.txtAgregar);
+		btnA = (Button) findViewById(R.id.btnAgregar);
 	}
 	public void estadoListView(){
-		String[] paises={"Argentina","Chile","Paraguay","Bolivia","Peru",
-				"Ecuador","Brasil","Colombia","Venezuela","Uruguay","Brasil","Colombia","Venezuela","Uruguay","Brasil","Colombia","Venezuela","Uruguay"};
+
+		paises.add("Argentina");
 		String[] habitantes={"40000000","17000000","6500000","10000000","30000000",
 				"14000000","183000000","44000000","29000000","3500000"};
-		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, paises);
+		arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, paises);
 		lvP.setAdapter(arrayAdapter);
+		lvP.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				tvH.setText("Poblacion de " + lvP.getItemAtPosition(position) + "es habotantes: "+habitantes[position]);
 
+			}
+		});
+
+	}
+
+	//estado de boton de agregar
+	public void agragrPaises(){
+		btnA.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				paises.add(txtA.getText().toString());
+				lvP.setAdapter(arrayAdapter);
+			}
+		});
 	}
 }
