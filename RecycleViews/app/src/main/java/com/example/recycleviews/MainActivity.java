10841d86505e12e6d622e5dc73d082293ced7f70
Rodri Quelali <rodriquelali@gmail.com>
2024-06-05 13:04:21 -0400

    recyrcleyView parte 2

--- RecycleViews/app/src/main/java/com/example/recycleviews/MainActivity.java
+++ RecycleViews/app/src/main/java/com/example/recycleviews/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.recycleviews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+import com.example.recycleviews.adapter.ListaAdapterCliente;
+import com.example.recycleviews.model.Cliente;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView listaClientes;
+    private ArrayList<Cliente> clientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarR();
+        cargarRecyrcleyView();
+    }
+    //clase R
+    public void cargarR(){
+        listaClientes = (RecyclerView) findViewById(R.id.recListaPersona);
+    }
+    //recyrcleyView
+    public void cargarRecyrcleyView(){
+        listaClientes.setLayoutManager(new LinearLayoutManager(this));
+        clientes = new ArrayList<Cliente>();
+        clientes.add(new Cliente("MAMANI", 1025666));
+        clientes.add(new Cliente("BRITO", 10256345));
+        ListaAdapterCliente listaAdapterCliente = new ListaAdapterCliente(clientes);
+        listaClientes.setAdapter(listaAdapterCliente);
+
     }
 }
--- RecycleViews/app/src/main/java/com/example/recycleviews/adapter/ListaAdapterCliente.java
+++ RecycleViews/app/src/main/java/com/example/recycleviews/adapter/ListaAdapterCliente.java
@@ -4,6 +4,7 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -27,10 +28,12 @@ public class ListaAdapterCliente extends RecyclerView.Adapter<ListaAdapterClient
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_cliente,null,false);
         return new ListaClienteViewHolder(view);
     }
-//id, identificadores de la clase R
+//id, identificadores de la clase R, pasos de parametros
     @Override
     public void onBindViewHolder(@NonNull ListaAdapterCliente.ListaClienteViewHolder holder, int position) {
-
+        //llamar lista Clientes;
+        holder.txtR.setText(clientes.get(position).getRazonSocial());
+        holder.txtN.setText(clientes.get(position).getNit());
     }
 
     //el tamano de la lista
@@ -39,9 +42,14 @@ public class ListaAdapterCliente extends RecyclerView.Adapter<ListaAdapterClient
         return clientes.size();
     }
 
+
+    //vamos a colocar la puente entre clase R y xml
     public class ListaClienteViewHolder extends RecyclerView.ViewHolder {
+        TextView txtR, txtN;
         public ListaClienteViewHolder(@NonNull View itemView) {
-            super(itemView);
+        super(itemView);
+            txtR = itemView.findViewById(R.id.txtRazon);
+            txtN = itemView.findViewById(R.id.txtNit);
         }
     }
 }
